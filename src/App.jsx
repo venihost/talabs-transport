5856a5171db0336644d7af665e64ed1d260c8a15
venihost <info.venihost@gmail.com>
2025-02-12 04:47:50 +0100

    home update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from './pages/home/Home';
 
 function App() {
   return (
