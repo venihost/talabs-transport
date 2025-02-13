04d590db4eb1fb98765f20f5d0075b0dcbabdab7
venihost <info.venihost@gmail.com>
2025-02-13 03:10:02 +0100

    update main.jsx
    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root');
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
