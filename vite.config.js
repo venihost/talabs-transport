021bd86a400d535e44b683ca11ad5a86105ac7bf
venihost <info.venihost@gmail.com>
2025-02-13 09:50:06 +0100

    com update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/talabs-transport/',
-  // ... rest of the config
-})})
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx'
+      }
+    }
+  }
+})
